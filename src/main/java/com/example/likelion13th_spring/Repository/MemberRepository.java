8049e294b567d5818001eedfd2d1c687d1f9c537
chaeyeonlee898 <chaeyeonlee898@gmail.com>
2025-08-03 17:05:05 +0900

    18주차 세션 실습

--- src/main/java/com/example/likelion13th_spring/Repository/MemberRepository.java
+++ src/main/java/com/example/likelion13th_spring/Repository/MemberRepository.java
@@ -1,4 +1,13 @@
 package com.example.likelion13th_spring.Repository;
 
-public class MemberRepository {
+import com.example.likelion13th_spring.domain.Member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+//Sprint Date JPA가 자동으로 인식해서 내부적으로 JPQL 쿼리를 생성해줌!
+//delete, save 등의 메서드는 이미 JpaRepository 안에 포함되어있음
+public interface MemberRepository extends JpaRepository<Member, Long>{
+    Optional<Member> findByName(String name);
+    Optional<Member> findByEmail(String email);
 }
